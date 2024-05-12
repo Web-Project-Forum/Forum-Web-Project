2535456c4f70e1ab3c38dd637f1fb76305f93720
petrovradostin <radostin.petrov15@gmail.com>
2024-05-12 22:03:47 +0300

    Add tests for users_service and category_service

---
+++ server/tests/category_service_test.py
@@ -0,0 +1,97 @@
+import unittest
+from unittest.mock import patch
+from data.models import Category
+from services import category_service
+
+
+def fake_category():
+    category = Category(
+        id = 1,
+        name = "Category Name",
+        is_private = 0,
+        is_locked = 0)
+    return category
+
+class CategoryServiceTests(unittest.TestCase):
+
+    @patch('services.category_service.read_query')
+    def test_all(self, mock_read_query):
+        # Arrange
+        mock_read_query.return_value = [(1, 'Category Name', 0, 0)]
+        # Act
+        categories = category_service.all(search=None, skip=0, take=1)
+        expected_category = fake_category()
+        # Assert
+        self.assertEqual([expected_category], list(categories))
+
+    
+    @patch('services.category_service.read_query')
+    def test_get_by_id(self, mock_read_query):
+        # Arrange
+        mock_read_query.return_value = [(1, 'Category Name', 0, 0)]
+        # Act
+        category = category_service.get_by_id(1)
+        expected_category = fake_category()
+        # Assert
+        self.assertEqual(expected_category, category)
+
+
+    @patch('services.category_service.read_query')
+    def test_exists(self, mock_read_query):
+        # Arrange
+        mock_read_query.return_value = [(1, 'Category Name', 0)]
+        # Act
+        exists = category_service.exists(1)
+        # Assert
+        self.assertTrue(exists)
+
+
+    @patch('services.category_service.insert_query')
+    def test_create(self, mock_insert_query):
+        # Arrange
+        mock_insert_query.return_value = 1  
+        category = Category(id = 1, name='Category Name', is_private=0, is_locked=0)
+        
+        # Act
+        created_category = category_service.create(category)
+        expected_output = fake_category()
+        # Assert
+        self.assertEqual(expected_output, created_category)
+
+    
+    @patch('services.category_service.update_query')
+    def test_delete(self, mock_update_query):
+        # Arrange
+        category_id = 1
+        # Act
+        category_service.delete(category_id)
+        # Assert
+        # Check if update_query was called twice, once for each deletion query
+        mock_update_query.assert_any_call('DELETE FROM categories WHERE categories_id = ?', (category_id,))
+        mock_update_query.assert_any_call('DELETE FROM categories WHERE id = ?', (category_id,))
+
+
+    @patch('services.category_service.update_query')
+    def test_update_category(self, mock_update_query):
+        # Arrange
+        mock_update_query.return_value = [(1, 'Category Name', 0, 0)]
+        # Act
+        old_category = Category(id=1, name='Old Name', is_private=0, is_locked=0)
+        new_category = Category(id=1, name='New Name', is_private=0, is_locked=0)
+        categories = category_service.update(old_category, new_category)
+        expected_topic = new_category
+        # Assert
+        self.assertEqual(expected_topic, categories)
+
+
+    @patch('services.category_service.read_query')
+    def test_get_private_categories(self, mock_read_query):
+        # Arrange
+        user_id = 1
+        mock_read_query.return_value = [(1,), (2,), (3,)]
+        # Act
+        private_categories = category_service.get_private_categories(user_id)
+        mock_read_query.assert_called_once_with(
+            'SELECT category_id from permissions where user_id = ?', (user_id,))
+        # Assert
+        self.assertEqual(private_categories, {1, 2, 3})
---
+++ server/tests/users_service_test.py
@@ -0,0 +1,63 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+import jwt
+from data.models import User
+from services import users_service
+
+
+def fake_user():
+    user = User(
+        id = 1,
+        username = "name",
+        password = "password",
+        role = "user",
+        email = "email@abc.com")
+    return user
+
+class UsersServiceTests(unittest.TestCase):
+
+    @patch('services.users_service.read_query')
+    def test_find_by_username(self, mock_read_query):
+        # Arrange
+        mock_read_query.return_value = [(1, 'name', 'password', 'user', 'email@abc.com')]
+        # Act
+        user = users_service.find_by_username('name')
+        # Assert
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'name')
+
+
+    @patch('services.users_service.insert_query')
+    def test_create(self, mock_insert_query):
+        # Arrange
+        mock_insert_query.return_value = 1  # generated_id
+        # Act
+        user = users_service.create('name', 'password', 'test@example.com')
+        # Assert
+        self.assertEqual(user.username, 'name')
+
+
+    @patch('services.users_service.jwt.encode')
+    def test_create_token(self, mock_jwt_encode):
+        # Arrange
+        mock_jwt_encode.return_value = 'encoded_token'
+        user = User(id=1, username='name', password='password', role='user', email='email@abc.com')
+        # Act
+        token = users_service.create_token(user)
+        # Assert
+        self.assertEqual(token, 'encoded_token')
+
+
+    @patch('services.users_service.jwt.decode')
+    def test_is_authenticated(self, mock_jwt_decode):
+        # Arrange
+        mock_jwt_decode.return_value = {'id': 1, 'username': 'test_user'}
+        # Act
+        mock_read_query = MagicMock()
+        mock_read_query.return_value = [(1,)]
+        # Assert
+        with patch('services.users_service.read_query', mock_read_query):
+            self.assertTrue(users_service.is_authenticated('valid_token'))
+
+    
